e8f4d5cc1d89a8cfb7faee610f52e7c18f2d117e
zensei76 <bhargavburgul@gmail.com>
2023-08-09 16:42:43 +0530

    Optmized for mobile devices

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,37 +1,50 @@
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 import './index.scss'
 import LogoBB from '../../assets/images/logoBB.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons'
+import {
+  faBars,
+  faClose,
+  faEnvelope,
+  faHome,
+  faSuitcase,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons'
 import {
   faGithub,
   faLinkedinIn,
   faSkype,
 } from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
+import Button from '@mui/material/Button'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import { IconButton } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
 
 const Sidebar = () => {
+  // const [showNav, setShowNav] = useState(false)
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
 
+  const navigate = useNavigate()
 
   return (
     <div className="nav-bar">
-      <Link 
-        className="logo" 
-        to="/">
-
+      <Link className="logo" to="/" >
         <img src={LogoBB} alt="logo" />
         <img className="sub-logo" src={LogoSubtitle} alt="BhargavBurgul" />
-
       </Link>
-      <nav>
-
-        <NavLink 
-        exact="true" 
-        activeclassname="active" 
-        to="/">
-
-          <FontAwesomeIcon icon={faHome} color="#D2D6DC" />
-
+      <nav  >
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#e2314f" />
         </NavLink>
 
         <NavLink
@@ -40,18 +53,11 @@ const Sidebar = () => {
           className="about-link"
           to="/about"
         >
-
-          <FontAwesomeIcon icon={faUser} color="#D2D6DC" />
-          
+          <FontAwesomeIcon icon={faUser} color="#e2314f" />
         </NavLink>
 
-        <NavLink
-          activeclassname="active"
-          className="work-link"
-          to="/work"
-
-        >
-          <FontAwesomeIcon icon={faSuitcase} color="#D2D6DC" />
+        <NavLink activeclassname="active" className="work-link" to="/work">
+          <FontAwesomeIcon icon={faSuitcase} color="#e2314f" />
         </NavLink>
 
         <NavLink
@@ -60,7 +66,14 @@ const Sidebar = () => {
           className="contact-link"
           to="/contact"
         >
-          <FontAwesomeIcon icon={faEnvelope} color="#D2D6DC" />
+          <FontAwesomeIcon icon={faEnvelope} color="#e2314f" />
+
+          <FontAwesomeIcon
+            icon={faClose}
+            color="#ffd700"
+            size="3x"
+            className="close-icon"
+          />
         </NavLink>
       </nav>
       <ul>
@@ -70,7 +83,7 @@ const Sidebar = () => {
             rel="noreferrer"
             href="https://www.linkedin.com/in/bhargav-burgul/"
           >
-            <FontAwesomeIcon icon={faLinkedinIn} color="#D2D6DC" />
+            <FontAwesomeIcon icon={faLinkedinIn} color="#e2314f" />
           </a>
         </li>
         <li>
@@ -79,15 +92,73 @@ const Sidebar = () => {
             rel="noreferrer"
             href="https://github.com/zensei76"
           >
-            <FontAwesomeIcon icon={faGithub} color="#D2D6DC" />
+            <FontAwesomeIcon icon={faGithub} color="#e2314f" />
           </a>
         </li>
         <li>
           <a target="_blank" rel="noreferrer" href=" ">
-            <FontAwesomeIcon icon={faSkype} color="#D2D6DC" />
+            <FontAwesomeIcon icon={faSkype} color="#e2314f" />
           </a>
         </li>
       </ul>
+
+      <div className='show-on-mobile'>
+        <IconButton
+          className="hamburger-icon"
+          id="basic-button"
+          aria-controls={open ? 'basic-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}
+          sx={{ color: '#e2314f' }}
+        >
+          <MenuIcon fontSize="large" />
+        </IconButton>
+
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+        >
+          <MenuItem
+            onClick={() => {
+              setAnchorEl(null)
+              navigate('/')
+            }}
+          >
+            Home
+          </MenuItem>
+
+          <MenuItem
+            onClick={() => {
+              setAnchorEl(null)
+              navigate('/about')
+            }}
+          >
+            About
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              setAnchorEl(null)
+              navigate('/work')
+            }}
+          >
+            My Work
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              setAnchorEl(null)
+              navigate('/contact')
+            }}
+          >
+            Contact
+          </MenuItem>
+        </Menu>
+      </div>
     </div>
   )
 }
